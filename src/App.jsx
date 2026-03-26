819915132471c95c15374a61723e35d525337154
Trithereon <eliottrassa@gmail.com>
2026-03-26 18:21:00 -0400

    Add practice content

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Greeting from "./Greeting";
+import Test from "./Test";
+
+function List(props) {
+  return (
+    <>
+      {!props.animals ? (
+        <div>Loading...</div>
+      ) : props.animals.length > 0 ? (
+        <ul>
+          {props.animals.map((animal) => {
+            return <li key={animal}>{animal}</li>;
+          })}
+        </ul>
+      ) : (
+        <div>There are no animals in the list!</div>
+      )}
+    </>
+  );
+}
+
+function App() {
+  const animals = [];
+  return (
+    <>
+      <h1>Animals:</h1>
+      <List animals={animals} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Greeting.jsx
@@ -0,0 +1,11 @@
+function Greeting() {
+  return (
+    <>
+      <h2>"Hello, I love my girlfriend dearly."</h2>
+      <img width="600px" src="./src/assets/daph.jpg" alt="" />
+      <video controls width="600px" src="./src/assets/cats.webm"></video>
+    </>
+  );
+}
+
+export default Greeting;
